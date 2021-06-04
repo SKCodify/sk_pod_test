b52f9ef1b130b344ad43784ea6fb9356511d2ae9
Shubham khare <shkhare@mafcarrefour.com>
2021-06-04 17:20:06 +0530

    Added test cases

---
+++ SK_POD_TEST/SKPodtest.swift
@@ -0,0 +1,22 @@
+//
+//  SKPodtest.swift
+//  SK_POD_TEST
+//
+//  Created by Shubham Khare on 04/06/21.
+//
+
+import Foundation
+
+public final class SKPodtest {
+
+    let name = "SKPodtest"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
